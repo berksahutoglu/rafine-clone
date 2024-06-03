e8f21156a9f9cf69dad5781af3238176d71a2c01
Berk Şahutoğlu <berksahutogl@gmail.com>
2024-06-03 22:06:11 +0300

    .

--- backend/connect.js
+++ backend/connect.js
@@ -6,3 +6,11 @@ export const db = mysql.createConnection({
   password: "1234",
   database: "rafine",
 });
+
+db.connect((err) => {
+  if (err) {
+    console.error("Database connection failed:", err.stack);
+    return;
+  }
+  console.log("Connected to database.");
+});
