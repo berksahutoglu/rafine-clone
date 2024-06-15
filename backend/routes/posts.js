009a62f2ae284a7be719bff6234823c3f8c58d9d
Berk Şahutoğlu <berksahutogl@gmail.com>
2024-06-15 18:27:51 +0300

    .

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,5 +1,4 @@
 import express from "express";
-import { verifyToken } from "../middleware/authMiddleware.js";
 
 import {
   addPost,
@@ -10,9 +9,9 @@ import {
 
 const router = express.Router();
 
-router.get("/",verifyToken, getPosts);
-router.post("/",verifyToken, addPost);
-router.put("/:id",verifyToken, updatePost);
-router.delete("/:id",verifyToken, deletePost);
+router.get("/", getPosts);
+router.post("/", addPost);
+router.put("/:id", updatePost);
+router.delete("/:id", deletePost);
 
 export default router;
