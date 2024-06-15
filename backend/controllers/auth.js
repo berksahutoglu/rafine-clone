433ecc2eb865e787fd075ea9bbabdcf69e8034e7
Berk Şahutoğlu <115134027+berksahutoglu@users.noreply.github.com>
2024-06-15 18:14:48 +0300

    Update auth.js

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -4,7 +4,7 @@ import jwt from "jsonwebtoken";
 
 export const register = async (req, res) => {
   try {
-    const emailRegex = /^[^\s@]+@[^\s@]+.[^\s@]+$/;
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailRegex.test(req.body.email)) {
       return res.status(400).json("Geçerli bir adres girin.");
     }
@@ -33,22 +33,25 @@ export const login = async (req, res) => {
     const user = await User.findOne({ email: req.body.email });
     if (!user) return res.status(404).json("User not found!");
 
-    const isPasswordValid = bcrypt.compareSync(req.body.password, user.password);
+    const isPasswordValid = bcrypt.compareSync(
+      req.body.password,
+      user.password
+    );
     if (!isPasswordValid)
       return res.status(400).json("Wrong password or email!");
 
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    const token = jwt.sign({ id: user._id }, "secretkey", { expiresIn: "1h" });
 
     const { password, ...others } = user._doc;
 
     res
       .cookie("accessToken", token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
+        secure: true, // production'da true olmalı
         sameSite: "none",
       })
       .status(200)
-      .json({ ...others, token });
+      .json(others);
   } catch (err) {
     return res.status(500).json(err);
   }
@@ -57,7 +60,7 @@ export const login = async (req, res) => {
 export const logout = (req, res) => {
   res
     .clearCookie("accessToken", {
-      secure: process.env.NODE_ENV === "production",
+      secure: true, // production'da true olmalı
       sameSite: "none",
     })
     .status(200)
