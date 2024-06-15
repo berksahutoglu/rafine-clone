d728db9ab466b0bdb1780e0c6da95a64fee0b38a
Berk Şahutoğlu <115134027+berksahutoglu@users.noreply.github.com>
2024-06-15 18:15:07 +0300

    Update authMiddleware.js

--- backend/controllers/authMiddleware.js
+++ backend/controllers/authMiddleware.js
@@ -4,7 +4,7 @@ export const verifyToken = (req, res, next) => {
   const token = req.cookies.accessToken;
   if (!token) return res.status(401).json("Not authenticated!");
 
-  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+  jwt.verify(token, "secretkey", (err, user) => {
     if (err) return res.status(403).json("Token is not valid!");
     req.user = user;
     next();
