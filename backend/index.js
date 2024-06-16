9b3079b1b29194e78a8629cfc418ddbf396b7322
Berk Şahutoğlu <115134027+berksahutoglu@users.noreply.github.com>
2024-06-16 12:51:21 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,7 @@ import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
 import authRoutes from "./routes/auth.js";
 import notificationRoutes from "./routes/notification.js";
+import jwt from "jsonwebstoken";
 
 // Load environment variables
 dotenv.config();
